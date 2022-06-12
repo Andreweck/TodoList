f9d0335b2a20581aec85e021af587abdd29eacf1
Andrew Eckelmann <37353397+Andreweck@users.noreply.github.com>
2022-06-12 21:37:17 +0000

    Initial commit

---
+++ mainMenu.js
@@ -0,0 +1,42 @@
+const input = require('readline-sync');
+const Database = require("@replit/database");
+//const task = require("./tasks.js");
+const { Task, addTask, displayTasks } = require('./tasks.js');
+//const mainMenu = require('./index.js')
+
+
+function mainMenu(db){
+
+let optionList = ["1: Add Task", "2: Reference/Edit", "3: Clear Tasks" ];
+console.log("Options: (Type only the number of your choice!!!)");
+for (let i = 0; i < optionList.length; i++){
+  console.log(optionList[i]);
+}
+
+var int1;
+let choice1 = input.question("Choice:");
+int1 = Number(choice1) - 1;
+
+if (optionList[int1] === undefined){
+  console.log("Out of Range!!")
+}
+else{
+  for (let i = 0; i < optionList.length; i++){
+    if (choice1 === optionList[i][0]){
+      console.log(optionList[int1]);
+    }
+  }
+}
+if (choice1 === "1"){
+  addTask(db);
+
+  
+}
+else if(choice1 === "2"){
+
+  displayTasks(db);
+  
+
+}
+}
+module.exports = mainMenu;
---
+++ tasks.js
@@ -0,0 +1,68 @@
+const input = require('readline-sync');
+const Database = require("@replit/database");
+
+
+
+const mainMenu= require("./index.js");
+
+class Task{
+  constructor(id, name, description, timeAdded, isComplete, timeCompleted){
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.timeAdded = timeAdded;
+    this.isComplete = isComplete;
+    this.timeCompleted = timeCompleted;
+
+}
+}
+
+function addTask(db){
+  db.getAll().then(keys => {
+  let int;
+  let taskName = input.question("What is the title of this task?");
+  let taskDescription = input.question("Please give a description of this task:");
+  let taskList = new Task(Object.keys(keys).length + 1, taskName, taskDescription, Date("Central"), false, null);
+    db.set(taskList.id, [taskList.name, taskList.description, taskList.timeAdded, taskList.isComplete, taskList.timeCompleted]);
+ 
+  }); 
+  
+}
+function displayTasks(db){
+  db.getAll().then(keys => {console.log(keys)
+  let choice2 = input.question("Which task do you wish to select?");
+  
+                        
+if(Number(choice2) <= keys.length){db.get(choice2).then(keys => {console.log(keys);
+                             let nme = keys[0];
+                             let desc = keys[1];
+                             let time = keys[2];
+                             console.log("What do you wish to do with this?\n1: Complete Task\n2: Edit Task\n3: Delete Task\n4: Go Back");
+                             let choice3 = input.question();
+choice3 = Number(choice3);
+                              if (choice3 === 1){
+                             db.set(choice2, [nme, "GOOD JOBBB!!!!",                               time, true, Date()]);
+                              }
+                              else if (choice3 === 2){
+                                
+                              }
+                              else if (choice3 === 3){
+                             db.delete(choice2);
+                             mainMenu(db);
+                              }
+                             
+                             
+                             });}
+else{console.log("Out of range!")}
+
+/*finally{
+  mainMenu(db);
+}  */                         
+                          
+                           }); 
+  
+}
+module.exports = {Task: Task,
+                 addTask: addTask,
+                 displayTasks: displayTasks};
+  //{id: }
